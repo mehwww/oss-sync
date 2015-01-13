c2ae6331d0b0a7204bc24ea7bcf32b05203fefcb
meh <mehwww7@gmail.com>
2015-01-14 06:59:15 +0800

    Promisify all code, need clean

--- bin/osync.js
+++ bin/osync.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var path = require('path');
 
+var Promise = require('bluebird');
 var program = require('commander');
 var chalk = require('chalk');
 
@@ -21,24 +22,38 @@ var configFile = program.args[0] || '.oss-sync.json';
 try {
   var config = require(path.resolve(process.cwd(), configFile))
 } catch (e) {
-  console.log('');
+  console.log();
   console.log(chalk.red('    Failed to load config file'));
-  console.log('');
+  console.log();
   process.exit();
 }
 
 var sync = ossSync(config);
 
-sync.exec(function (err, msg) {
-  if (err) {
-    console.log('');
-    console.log('    ' + chalk.bgRed('Error occurred:'));
-    console.log('');
-    console.log('    ' + chalk.red(err));
-    console.log('');
-  } else {
-    console.log('');
+//sync.exec(function (err, msg) {
+//  if (err) {
+//    console.log();
+//    console.log('    ' + chalk.bgRed('Error occurred:'));
+//    console.log();
+//    console.log('    ' + chalk.red(err));
+//    console.log();
+//  } else {
+//    console.log();
+//    console.log('    ' + chalk.cyan('Sync completed'));
+//    console.log();
+//  }
+//})
+
+sync.exec()
+  .then(function () {
+    console.log();
     console.log('    ' + chalk.cyan('Sync completed'));
-    console.log('');
-  }
-})
+    console.log();
+  })
+  .catch(function (err) {
+    console.log();
+    console.log('    ' + chalk.bgRed('Error occurred:'));
+    console.log();
+    console.log('    ' + chalk.red(err));
+    console.log();
+  })
