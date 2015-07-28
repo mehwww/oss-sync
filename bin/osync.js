c76baf99418c031b227d35e253df972a939d15a6
meh <mehwww7@gmail.com>
2015-07-28 23:34:07 +0800

    Force upload feature

--- bin/osync.js
+++ bin/osync.js
@@ -9,17 +9,19 @@ var ossSync = require('../lib/sync');
 program
   .description('Sync Aliyun OSS bucket')
   .usage('<config file ...>')
+  .option('-f, --force-upload', 'Force upload all files')
   .version(require('../package.json').version)
 
 program.parse(process.argv);
 
 if (program.args.length > 1) {
-  program.help()
+  return program.help()
 }
 
 var configFile = program.args[0] || '.oss-sync.json';
 try {
   var config = require(path.resolve(process.cwd(), configFile))
+  if (program.forceUpload) config.forceUpload = program.forceUpload
 } catch (e) {
   console.log();
   console.log(chalk.red('    Failed to load config file'));
